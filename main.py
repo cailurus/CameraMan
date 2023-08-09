3f8087518345029d2b6cc15ccf9051296a8eb5fd
More Zhou <morezhou@Mores-MBP.guest.corp.microsoft.com>
2023-08-09 13:38:04 +0800

    update web portal

--- main.py
+++ main.py
@@ -15,28 +15,24 @@ WIDTH = 1280
 HEIGHT = 720
 FPS = 24.0
 
-# 必须指定CAP_DSHOW(Direct Show)参数初始化摄像头,否则无法使用更高分辨率
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-# 设置摄像头设备分辨率
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
-# 设置摄像头设备帧率,如不指定,默认600
 cap.set(cv2.CAP_PROP_FPS, FPS)
 
 
 gesture_model = GestureModel()
 face_model = FaceModel()
 
-
 video = cv2.VideoCapture(0)
-
 timestamp = 0
-
 prev_frame_time, new_frame_time = 0, 0
 
 
 while video.isOpened():
     ret, frame = video.read()
+    print(type(frame))
+    print(frame)
 
     if not ret:
         print("Ignoring empty frame")
@@ -54,11 +50,8 @@ while video.isOpened():
     face_res = face_model.inference(mp_image, timestamp)
 
     annotated_image = put_text(frame, fps, (0, 50))
-
     annotated_image = draw_hand_landmarks_on_image(annotated_image, hand_result)
-
     annotated_image = draw_landmarks_on_image(annotated_image, face_res)
-
     cv2.imshow("show", annotated_image)
 
     if cv2.waitKey(5) & 0xFF == ord("q"):
--- web_portal.py
+++ web_portal.py
@@ -1,10 +1,60 @@
 import streamlit as st
+import cv2
+import av
+import mediapipe as mp
+import time
+from streamlit_webrtc import webrtc_streamer
+from cameraman.utils import (
+    put_text,
+    draw_landmarks_on_image,
+    draw_hand_landmarks_on_image,
+)
+
+from cameraman.model import GestureModel, FaceModel
+
+gesture_model = GestureModel()
+face_model = FaceModel()
 
 
-st.title("Uber pickups in NYC")
+st.title("My first Streamlit app")
+st.write("Hello, world")
 
 
-picture = st.camera_input("Take a picture")
+timestamp = 0
+prev_frame_time = 0
+new_frame_time = 0
 
-if picture:
-    st.image(picture)
+
+def callback(frame):
+    global timestamp
+    global prev_frame_time
+    timestamp += 1
+
+    new_frame_time = time.time()
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+
+    frame = frame.to_ndarray(format="bgr24")
+    #
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    hand_result = gesture_model.inference(mp_image, timestamp)
+    face_res = face_model.inference(mp_image, timestamp)
+    #
+    annotated_image = put_text(frame, fps, (0, 50))
+    annotated_image = draw_hand_landmarks_on_image(annotated_image, hand_result)
+    annotated_image = draw_landmarks_on_image(annotated_image, face_res)
+
+    # return annotated_image
+
+    return av.VideoFrame.from_ndarray(annotated_image, format="bgr24")
+
+
+webrtc_streamer(
+    key="example",
+    video_frame_callback=callback,
+    media_stream_constraints={
+        "video": {
+            "width": 1920,
+        }
+    },
+)
