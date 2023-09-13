70f929b5961d4f98101bdc61f11744060b6f53d5
More Zhou <morezhou@Mores-MBP.guest.corp.microsoft.com>
2023-09-13 18:36:16 +0800

    update web portal

--- pygame_legacy_crop.py
+++ pygame_legacy_crop.py
@@ -5,6 +5,7 @@ import statistics
 import cv2
 import numpy as np
 import math
+
 import tensorflow as tf
 from cameraman.model import (
     SoundModel,
@@ -14,6 +15,13 @@ from cameraman.utils import parse_audio_result
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
+mp_hands = mp.solutions.hands
+hand_model = mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+)
+
 model0 = mp_face_mesh.FaceMesh(
     max_num_faces=2,
     refine_landmarks=True,
@@ -30,20 +38,6 @@ import time
 from mediapipe.tasks.python.components import containers
 from mediapipe.tasks.python.audio.core import audio_record
 
-sound_model = SoundModel()
-buffer_size, sample_rate, num_channels = 15600, 44100, 1
-audio_format = containers.AudioDataFormat(num_channels, sample_rate)
-record = audio_record.AudioRecord(num_channels, sample_rate, buffer_size)
-audio_data = containers.AudioData(buffer_size, audio_format)
-
-input_length_in_second = (
-    float(len(audio_data.buffer)) / audio_data.audio_format.sample_rate
-)
-
-interval_between_inference = input_length_in_second * (1 - 0.5)
-pause_time = interval_between_inference * 0.1
-
-last_inference_time = time.time()
 
 pygame.init()
 
@@ -109,8 +103,8 @@ emotion = EmotionModel()
 
 def convert_coord_drop(face_location):
     from_x, from_y, to_x, to_y = face_location
-    margin = max(to_x - from_x, to_y - from_y) // 2
-    size = int(max(to_x - from_x, to_y - from_y) * 1.5)
+    margin = max(to_x - from_x, to_y - from_y)
+    size = int(max(to_x - from_x, to_y - from_y) * 3)
 
     new_from_x = max(from_x - margin, 0)
     new_from_y = max(from_y - margin, 0)
@@ -120,9 +114,42 @@ def convert_coord_drop(face_location):
     return [new_from_x, new_from_y, new_to_x - new_from_x, new_to_y - new_from_y]
 
 
+def convert_crop_location(face_location):
+    from_x, from_y, to_x, to_y = face_location
+    margin = max(to_x - from_x, to_y - from_y)
+    size = int(max(to_x - from_x, to_y - from_y) * 3)
+
+    new_from_x = max(from_x - margin, 0)
+    new_from_y = max(from_y - margin, 0)
+    new_to_x = min(new_from_x + size, 1024)
+    new_to_y = min(new_from_y + size, 576)
+
+    return [new_from_x, new_from_y, new_to_x, new_to_y]
+
+
+def hand_inference(image):
+    results = hand_model.process(image)
+    num_hands = 0
+
+    if results.multi_hand_landmarks:
+        num_hands = len(results.multi_hand_landmarks)
+        # for hand_landmarks in results.multi_hand_landmarks:
+        #     mp_drawing.draw_landmarks(
+        #         image,
+        #         hand_landmarks,
+        #         mp_hands.HAND_CONNECTIONS,
+        #         mp_drawing_styles.get_default_hand_landmarks_style(),
+        #         mp_drawing_styles.get_default_hand_connections_style(),
+        #     )
+    # Flip the image horizontally for a selfie-view display.
+
+    return num_hands
+
+
 def face_inference(face_mesh_model, img):
     face_locations = []
     emotions = []
+    hands_info = []
     view = pygame.surfarray.array3d(img)
     view = view.transpose([1, 0, 2])
     # image = cv2.cvtColor(view, cv2.COLOR_RGB2BGR)
@@ -142,31 +169,47 @@ def face_inference(face_mesh_model, img):
             to_x = int(max(x_coordinates) * width)
             to_y = int(max(y_coordinates) * height)
             face_locations.append((from_x, from_y, to_x, to_y))
-
             image = cv2.rectangle(image, (from_x, from_y), (to_x, to_y), (255, 0, 0), 2)
+
+            new_from_x, new_from_y, new_to_x, new_to_y = convert_crop_location(
+                (from_x, from_y, to_x, to_y)
+            )
+
+            croped_image = image[new_from_y:new_to_y, new_from_x:new_to_x]
+
+            num_hands = hand_inference(croped_image)
+
+            hands_info.append(num_hands)
+
             lm_list = [[landmark.x, landmark.y] for landmark in face_landmarks.landmark]
             lm_list_flat = [item for sublist in lm_list for item in sublist]
             emotion_result = emotion.inference(lm_list_flat)
             emotions.append(emotion_result)
     image = image.transpose([1, 0, 2])
-    return image, face_locations, emotions
+    return image, face_locations, emotions, hands_info
 
 
 while True:
     img0 = cam0.get_image()
     img0 = pygame.transform.flip(img0, True, False)
-    audio_raw = record.read(15600)
+    #   audio_raw = record.read(15600)
 
-    sound_result = sound_model.inference(audio_raw, timestamp)
-    sound_result_str = parse_audio_result(sound_result)
+    #     sound_result = sound_model.inference(audio_raw, timestamp)
+    #     sound_result_str = parse_audio_result(sound_result)
 
-    annotated_image_0, face_locations, face_emotions = face_inference(model0, img0)
+    annotated_image_0, face_locations, face_emotions, hands_info = face_inference(
+        model0, img0
+    )
+    audio_result = open("audio_res.txt", "r").read()
+    audio_inference = []
+
     annotated_image_0 = pygame.surfarray.make_surface(annotated_image_0)
     gameDisplay.blit(annotated_image_0, (0, 0))
 
     # annotated_image_1, direction_score_1 = face_inference(model1, img1)
     # annotated_image_1 = pygame.surfarray.make_surface(annotated_image_1)
     # gameDisplay.blit(annotated_image_1, (1024, 0))
+
     if face_locations is not None and len(face_locations) > 0:
         for idx, face in enumerate(face_locations):
             face = convert_coord_drop(face)
@@ -177,6 +220,15 @@ while True:
                 font.render(f"DirectionScore: {face_emotions[idx]}", True, white),
                 (0 + idx * 576, 576),
             )
+            gameDisplay.blit(
+                font.render(f"Audio Classification: {audio_result}", True, white),
+                (0 + idx * 576, 586),
+            )
+            gameDisplay.blit(
+                font.render(f"Num Hands: {hands_info[idx]}", True, white),
+                (0 + idx * 576, 596),
+            )
+
     pygame.display.update()
 
     for event in pygame.event.get():
