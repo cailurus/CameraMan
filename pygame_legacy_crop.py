3dcc39b8c4dc0942238c1259a546c2c9d3ac329e
cailurus <ailurus.chow@gmail.com>
2023-09-14 10:01:04 +0800

    n

--- pygame_legacy_crop.py
+++ pygame_legacy_crop.py
@@ -114,7 +114,7 @@ def convert_coord_drop(face_location):
     return [new_from_x, new_from_y, new_to_x - new_from_x, new_to_y - new_from_y]
 
 
-def convert_crop_location(face_location):
+def convert_cropped_location(face_location):
     from_x, from_y, to_x, to_y = face_location
     margin = max(to_x - from_x, to_y - from_y)
     size = int(max(to_x - from_x, to_y - from_y) * 3)
@@ -123,8 +123,14 @@ def convert_crop_location(face_location):
     new_from_y = max(from_y - margin, 0)
     new_to_x = min(new_from_x + size, 1024)
     new_to_y = min(new_from_y + size, 576)
-
-    return [new_from_x, new_from_y, new_to_x, new_to_y]
+    return [
+        new_from_x,
+        new_from_y,
+        new_to_x,
+        new_to_y,
+        new_to_x - new_from_x,
+        new_to_y - new_from_y,
+    ]
 
 
 def hand_inference(image):
@@ -133,33 +139,20 @@ def hand_inference(image):
 
     if results.multi_hand_landmarks:
         num_hands = len(results.multi_hand_landmarks)
-        # for hand_landmarks in results.multi_hand_landmarks:
-        #     mp_drawing.draw_landmarks(
-        #         image,
-        #         hand_landmarks,
-        #         mp_hands.HAND_CONNECTIONS,
-        #         mp_drawing_styles.get_default_hand_landmarks_style(),
-        #         mp_drawing_styles.get_default_hand_connections_style(),
-        #     )
-    # Flip the image horizontally for a selfie-view display.
 
     return num_hands
 
 
 def face_inference(face_mesh_model, img):
-    face_locations = []
-    emotions = []
-    hands_info = []
+    face_inference_results = []
     view = pygame.surfarray.array3d(img)
     view = view.transpose([1, 0, 2])
-    # image = cv2.cvtColor(view, cv2.COLOR_RGB2BGR)
     image = view.copy()
     height, width, img_c = image.shape
     results = face_mesh_model.process(image)
 
     image.flags.writeable = True
     if results.multi_face_landmarks:
-        lm_list = []
         for face_landmarks in results.multi_face_landmarks:
             x_coordinates = [landmark.x for landmark in face_landmarks.landmark]
             y_coordinates = [landmark.y for landmark in face_landmarks.landmark]
@@ -168,38 +161,43 @@ def face_inference(face_mesh_model, img):
             from_y = int(min(y_coordinates) * height)
             to_x = int(max(x_coordinates) * width)
             to_y = int(max(y_coordinates) * height)
-            face_locations.append((from_x, from_y, to_x, to_y))
             image = cv2.rectangle(image, (from_x, from_y), (to_x, to_y), (255, 0, 0), 2)
 
-            new_from_x, new_from_y, new_to_x, new_to_y = convert_crop_location(
-                (from_x, from_y, to_x, to_y)
-            )
+            (
+                new_from_x,
+                new_from_y,
+                new_to_x,
+                new_to_y,
+                width,
+                height,
+            ) = convert_cropped_location((from_x, from_y, to_x, to_y))
 
             croped_image = image[new_from_y:new_to_y, new_from_x:new_to_x]
-
             num_hands = hand_inference(croped_image)
-
-            hands_info.append(num_hands)
 
             lm_list = [[landmark.x, landmark.y] for landmark in face_landmarks.landmark]
             lm_list_flat = [item for sublist in lm_list for item in sublist]
             emotion_result = emotion.inference(lm_list_flat)
-            emotions.append(emotion_result)
+
+            face_inference_results.append(
+                {
+                    "face_locations_from_size": (new_from_x, new_from_y, width, height),
+                    "face_emotion": emotion_result,
+                    "num_hands": num_hands,
+                }
+            )
     image = image.transpose([1, 0, 2])
-    return image, face_locations, emotions, hands_info
+    return image, face_inference_results
 
 
 while True:
     img0 = cam0.get_image()
     img0 = pygame.transform.flip(img0, True, False)
     #   audio_raw = record.read(15600)
-
     #     sound_result = sound_model.inference(audio_raw, timestamp)
     #     sound_result_str = parse_audio_result(sound_result)
 
-    annotated_image_0, face_locations, face_emotions, hands_info = face_inference(
-        model0, img0
-    )
+    annotated_image_0, cropped_inference_result = face_inference(model0, img0)
     audio_result = open("audio_res.txt", "r").read()
     audio_inference = []
 
@@ -210,14 +208,14 @@ while True:
     # annotated_image_1 = pygame.surfarray.make_surface(annotated_image_1)
     # gameDisplay.blit(annotated_image_1, (1024, 0))
 
-    if face_locations is not None and len(face_locations) > 0:
-        for idx, face in enumerate(face_locations):
-            face = convert_coord_drop(face)
-            croped_surface = img0.subsurface(face)
-            scaled_surface = pygame.transform.scale(croped_surface, (576, 576))
-            gameDisplay.blit(scaled_surface, (0 + idx * 576, 576))
+    if len(cropped_inference_result) > 0:
+        for idx, result in enumerate(cropped_inference_result):
+            croped_surface = img0.subsurface(result["face_locations_from_size"])
+            scaled_croped_surface = pygame.transform.scale(croped_surface, (576, 576))
+
+            gameDisplay.blit(scaled_croped_surface, (0 + idx * 576, 576))
             gameDisplay.blit(
-                font.render(f"DirectionScore: {face_emotions[idx]}", True, white),
+                font.render(f"DirectionScore: {result['face_emotion']}", True, white),
                 (0 + idx * 576, 576),
             )
             gameDisplay.blit(
@@ -225,7 +223,7 @@ while True:
                 (0 + idx * 576, 586),
             )
             gameDisplay.blit(
-                font.render(f"Num Hands: {hands_info[idx]}", True, white),
+                font.render(f"Num Hands: {result['num_hands']}", True, white),
                 (0 + idx * 576, 596),
             )
 
